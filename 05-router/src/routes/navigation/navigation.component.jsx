48bfb905d45fa615e7699fc7916f482995f013d2
SOunit <workbookwork7@gmail.com>
2022-04-28 10:48:36 -0700

    add navigation component

--- 05-router/src/routes/navigation/navigation.component.jsx
+++ 05-router/src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <div>
-      <div>
-        <h1>Navigation</h1>
+    <>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <div>Logo</div>
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            Shop
+          </Link>
+        </div>
       </div>
       <Outlet />
-    </div>
+    </>
   );
 };
 
