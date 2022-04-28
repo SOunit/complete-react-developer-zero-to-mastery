f98288e998cca51532b81f79451bfc20bc1c0c74
SOunit <workbookwork7@gmail.com>
2022-04-28 11:23:25 -0700

    add new project

---
+++ 06-auth-firebase/src/App.js
@@ -0,0 +1,16 @@
+import Home from "./routes/home/home.component";
+import { Route, Routes } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<div>Shop</div>} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
