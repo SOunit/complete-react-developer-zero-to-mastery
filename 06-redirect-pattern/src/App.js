a1258272d68c9e7fff3be01aa639dc6df832fd4a
SOunit <workbookwork7@gmail.com>
2022-04-28 17:13:04 -0700

    add new project

---
+++ 06-redirect-pattern/src/App.js
@@ -0,0 +1,18 @@
+import Home from "./routes/home/home.component";
+import { Route, Routes } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./components/sign-in/sign-in.component";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<div>Shop</div>} />
+        <Route path="sign-in" element={<SignIn />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
