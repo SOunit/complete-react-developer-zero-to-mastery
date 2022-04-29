eac5377bb1d04ca30160677f7e75b4d833d90176
SOunit <workbookwork7@gmail.com>
2022-04-29 16:48:27 -0700

    add product context

--- 09-context/src/index.js
+++ 09-context/src/index.js
@@ -7,13 +7,16 @@ import reportWebVitals from "./reportWebVitals";
 
 import "./index.scss";
 import { UserProvider } from "./contexts/user.context";
+import { ProductProvider } from "./contexts/product.context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductProvider>
+          <App />
+        </ProductProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
