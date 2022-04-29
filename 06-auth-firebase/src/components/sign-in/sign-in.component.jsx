e50310fd917bbbc8425fb5d20ebf46670ba596a2
SOunit <workbookwork7@gmail.com>
2022-04-28 17:15:12 -0700

    delete redirect sample

--- 06-auth-firebase/src/components/sign-in/sign-in.component.jsx
+++ 06-auth-firebase/src/components/sign-in/sign-in.component.jsx
@@ -1,27 +1,11 @@
-import React, { useEffect } from "react";
-
-import { getRedirectResult } from "firebase/auth";
+import React from "react";
 
 import {
-  auth,
   signInWithGooglePopup,
   createUserDocumentFromAuth,
-  signInWithGoogleRedirect,
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
-  useEffect(() => {
-    const aaa = async () => {
-      const response = await getRedirectResult(auth);
-
-      if (response) {
-        const userDocRef = await createUserDocumentFromAuth(response.user);
-      }
-    };
-
-    aaa();
-  }, []);
-
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
@@ -31,9 +15,6 @@ const SignIn = () => {
     <div>
       <div>SignIn</div>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-      <button onClick={signInWithGoogleRedirect}>
-        Sign in with Google Redirect
-      </button>
     </div>
   );
 };
