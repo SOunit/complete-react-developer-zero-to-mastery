6ca79102da949ab1ddfcb20082971d57162ab559
SOunit <workbookwork7@gmail.com>
2022-04-30 15:42:05 -0700

    change scss to styled-component

--- 11-styled-component/src/components/sign-in-form/sign-in-form.component.jsx
+++ 11-styled-component/src/components/sign-in-form/sign-in-form.component.jsx
@@ -5,7 +5,7 @@ import {
   signInAuthUserWithEmailAndPassword,
   signInWithGooglePopup,
 } from "../../utils/firebase/firebase.utils";
-import Button from "../button/button.component";
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 import FormInput from "../form-input/form-input.component";
 import "./sign-in-form.style.scss";
 
@@ -81,7 +81,7 @@ const SignInForm = () => {
           <Button type="submit">sign in</Button>
           <Button
             type="button"
-            buttonType={"google"}
+            buttonType={BUTTON_TYPE_CLASSES.google}
             onClick={signInWithGoogle}
           >
             Google sign in
