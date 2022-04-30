f392ee6e4753c38fb1f56224e331f83c8b042a1e
SOunit <workbookwork7@gmail.com>
2022-04-30 13:52:53 -0700

    add preview component

--- 10-firebase-database/src/routes/shop/shop.component.jsx
+++ 10-firebase-database/src/routes/shop/shop.component.jsx
@@ -1,5 +1,5 @@
-import React, { Fragment, useContext } from "react";
-import ProductCard from "../../components/product-card/product-card.component";
+import React, { useContext } from "react";
+import CategoryPreview from "../../components/category-preview/category-preview.component";
 import { CategoriesContext } from "../../contexts/categories.context";
 import "./shop.styles.scss";
 
@@ -7,20 +7,14 @@ const Shop = () => {
   const { categoriesMap } = useContext(CategoriesContext);
 
   return (
-    <>
+    <div className="shop-container">
       {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
         return (
-          <Fragment key={title}>
-            <h2>{title}</h2>
-            <div className="products-container">
-              {categoriesMap[title].map((product) => (
-                <ProductCard key={product.id} product={product} />
-              ))}
-            </div>
-          </Fragment>
+          <CategoryPreview key={title} title={title} products={products} />
         );
       })}
-    </>
+    </div>
   );
 };
 
