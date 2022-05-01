f54483764d9fb92b6383218c4bda99b25a74bc30
SOunit <workbookwork7@gmail.com>
2022-05-01 15:32:14 -0700

    add use-selector for user state

--- 14-redux/src/routes/navigation/navigation.component.jsx
+++ 14-redux/src/routes/navigation/navigation.component.jsx
@@ -1,10 +1,10 @@
 import { useContext } from "react";
+import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import { CartContext } from "../../contexts/cart.context";
-import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import {
   NavigationContainer,
@@ -12,10 +12,11 @@ import {
   NavLinks,
   LogoContainer,
 } from "./navigation.styles.jsx";
+import { selectCurrentUser } from "../../store/user/user.selector.js";
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
   const { isCartOpen } = useContext(CartContext);
+  const currentUser = useSelector(selectCurrentUser);
 
   const signOutHandler = async () => {
     await signOutUser();
