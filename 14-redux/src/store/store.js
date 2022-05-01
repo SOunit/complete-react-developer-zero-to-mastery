d8c84df6b1f6d307704474d84d59f96f7806217c
SOunit <workbookwork7@gmail.com>
2022-05-01 15:09:50 -0700

    add redux setup

---
+++ 14-redux/src/store/store.js
@@ -0,0 +1,9 @@
+import { applyMiddleware, compose, createStore } from "redux";
+import logger from "redux-logger";
+import { rootReducer } from "./root-reducer";
+
+const middleWares = [logger];
+const composedEnhancer = compose(applyMiddleware(...middleWares));
+
+// root-reducer
+export const store = createStore(rootReducer, undefined, composedEnhancer);
