2878582a4ad1163ee184fb4f1ee1f23dae7d16bd
SOunit <workbookwork7@gmail.com>
2022-05-02 15:06:00 -0700

    add reselect to category

--- 14-redux/src/store/categories/category.selector.js
+++ 14-redux/src/store/categories/category.selector.js
@@ -1,8 +1,23 @@
-export const selectCategoriesMap = (state) => {
-  console.log("selector fired");
-  return state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
-};
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+// memorize selector
+// if input returns new object, then output run
+export const selectCategories = createSelector(
+  // input
+  [selectCategoryReducer],
+  // output
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+// memorize selector
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+);
