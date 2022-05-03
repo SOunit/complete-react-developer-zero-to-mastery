61b8b23d85bad9cb97def3e4629633c9786c9d65
SOunit <workbookwork7@gmail.com>
2022-05-03 09:58:46 -0700

    add new project

---
+++ 17-redux-saga/src/App.js
@@ -0,0 +1,44 @@
+import { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import Authentication from "./components/authentication/authentication.component";
+import Shop from "./routes/shop/shop.component";
+import Checkout from "./routes/checkout/checkout.component";
+import {
+  createUserDocumentFromAuth,
+  getCategoriesAndDocuments,
+  onAuthStateChangedListener,
+} from "./utils/firebase/firebase.utils";
+import { setCurrentUser } from "./store/user/user.action";
+import { setCategoriesMap } from "./store/categories/category.action";
+import { useDispatch } from "react-redux";
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      dispatch(setCurrentUser(user));
+    });
+
+    return unsubscribe;
+  }, [dispatch]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop/*" element={<Shop />} />
+        <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<Checkout />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
---
+++ 17-redux-saga/src/store/store.js
@@ -0,0 +1,35 @@
+import logger from "redux-logger";
+import { applyMiddleware, compose, createStore } from "redux";
+import { rootReducer } from "./root-reducer";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import thunk from "redux-thunk";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whiteList: ["cart"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const middleWares = [
+  process.env.NODE_ENV !== "production" && logger,
+  thunk,
+].filter(Boolean);
+
+// if exist use window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+// if not use compose
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+// const middleWares = [loggerMiddleware];
+const composedEnhancer = composeEnhancer(applyMiddleware(...middleWares));
+
+// root-reducer
+export const store = createStore(persistedReducer, undefined, composedEnhancer);
+
+export const persistor = persistStore(store);
