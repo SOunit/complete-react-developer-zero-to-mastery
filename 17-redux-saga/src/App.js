0c24992e5b1bf0281708787d70c1717293ab6ad1
SOunit <workbookwork7@gmail.com>
2022-05-03 13:12:56 -0700

    add saga sample

--- 17-redux-saga/src/App.js
+++ 17-redux-saga/src/App.js
@@ -6,27 +6,14 @@ import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./components/authentication/authentication.component";
 import Shop from "./routes/shop/shop.component";
 import Checkout from "./routes/checkout/checkout.component";
-import {
-  createUserDocumentFromAuth,
-  getCategoriesAndDocuments,
-  onAuthStateChangedListener,
-} from "./utils/firebase/firebase.utils";
-import { setCurrentUser } from "./store/user/user.action";
-import { setCategoriesMap } from "./store/categories/category.action";
 import { useDispatch } from "react-redux";
+import { checkUserSession } from "./store/user/user.action";
 
 const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
-      if (user) {
-        createUserDocumentFromAuth(user);
-      }
-      dispatch(setCurrentUser(user));
-    });
-
-    return unsubscribe;
+    dispatch(checkUserSession());
   }, [dispatch]);
 
   return (
