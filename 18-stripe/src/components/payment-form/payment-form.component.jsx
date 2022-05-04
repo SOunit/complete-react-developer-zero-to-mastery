416eef43d76023703ba1b5ef911d9a621d045409
SOunit <workbookwork7@gmail.com>
2022-05-03 17:26:37 -0700

    add payment logic

--- 18-stripe/src/components/payment-form/payment-form.component.jsx
+++ 18-stripe/src/components/payment-form/payment-form.component.jsx
@@ -13,11 +13,38 @@ const PaymentForm = () => {
     event.preventDefault();
 
     if (!stripe || !elements) return;
+
+    const response = await fetch("/.netlify/functions/create-payment-intent", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ amount: 1000 }),
+    }).then((res) => res.json());
+
+    const {
+      paymentIntent: { client_secret },
+    } = response;
+
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: { name: "Test User" },
+      },
+    });
+
+    if (paymentResult.error) {
+      alert("failed", paymentResult.error);
+    } else {
+      if (paymentResult.paymentIntent.status === "succeeded") {
+        alert("Payment successful");
+      }
+    }
   };
 
   return (
     <PaymentFormContainer>
-      <FormContainer>
+      <FormContainer onSubmit={paymentHandler}>
         <h2>Credit Card Payment</h2>
         <CardElement />
         <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
