f7908cb537cfea032ef5dac0370e9af16a78301d
SOunit <workbookwork7@gmail.com>
2022-05-04 10:52:37 -0700

    add new project

---
+++ 20-type-crown/src/store/categories/category.selector.js
@@ -0,0 +1,28 @@
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+// memorize selector
+// if input returns new object, then output run
+export const selectCategories = createSelector(
+  // input
+  [selectCategoryReducer],
+  // output
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+// memorize selector
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+);
+
+export const selectCategoriesIsLoading = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.isLoading
+);
